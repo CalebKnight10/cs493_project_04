9d0dafbf34c960fedcf2424845813b0276ad8119
Caleb Knight <knightca@oregonstate.edu>
2023-06-02 19:37:52 -0700

    Add functionality for downloading jpeg and png file in media.js

--- api/media.js
+++ api/media.js
@@ -3,7 +3,51 @@ const { downloadPhotoById} = require("../models/photo");
 const { containsPhoto } = require("../models/photo");
 const router = Router()
 
+
+/*
+* Download jpg
+*/
 router.get('/photos/:id.jpg', async (req, res, next) => {
+  try {
+    if (!await containsPhoto(req.params.id)) {
+      res.status(404).send({
+        error: "Photo not found."
+      })
+      return
+    }
+    await downloadPhotoById(req.params.id, res)
+  } catch (err) {
+    console.error(err)
+    res.status(500).send({
+      error: "Unable to fetch photo."
+    })
+  }
+})
+
+/*
+* Download jpeg
+*/
+router.get('/photos/:id.jpeg', async (req, res, next) => {
+  try {
+    if (!await containsPhoto(req.params.id)) {
+      res.status(404).send({
+        error: "Photo not found."
+      })
+      return
+    }
+    await downloadPhotoById(req.params.id, res)
+  } catch (err) {
+    console.error(err)
+    res.status(500).send({
+      error: "Unable to fetch photo."
+    })
+  }
+})
+  
+/*
+* Donwnload png
+*/  
+router.get('/photos/:id.png', async (req, res, next) => {
   try {
     if (!await containsPhoto(req.params.id)) {
       res.status(404).send({
